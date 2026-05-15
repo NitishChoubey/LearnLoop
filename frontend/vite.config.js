ef4146e4f5804f6c69ade24824f4713b4403c8fa
Nitish Choubey <nitishchoubey1234@gmail.com>
2026-05-15 08:10:54 +0530

    Adding vite host preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      "learnloop-k071.onrender.com",
+    ],
+  },
   server: {
     port: 5173,
     proxy: {
